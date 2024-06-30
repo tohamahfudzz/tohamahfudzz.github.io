3f4573631f202ef48233f45d661991e5e7c0c8ef
tohamahfudzz <147677606+tohamahfudzz@users.noreply.github.com>
2024-06-30 15:37:13 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,71 @@
+const nama=document.getElementById('nama');
+const lampu=document.getElementsByClassName('lampu');
+const body=document.body;
+const deskrip=document.getElementsByClassName('deskripsi');
+const link=document.getElementsByClassName('link');
+const judulLink=link[0].querySelector(':nth-child(1)');
+const videoBG= document.getElementById('videoBG');
+
+lampu[0].style.backgroundImage="url('1.png')";
+body.style.backgroundColor="white";
+nama.style.color="black";
+//nama.style.fontSize="90px";
+nama.style.fontFamily="times new roman";
+nama.style.textAlign="center";
+
+
+//lampu.onclick=hidup;
+/*lampu[0].addEventListener('click',function()
+{
+lampu[0].classList.add('animate');
+});*/
+
+function aktif()
+{  
+	if(body.style.backgroundColor==="white")
+	{
+		lampu[0].classList.add('animate');
+	}
+
+	setTimeout(function()
+	{
+
+	if(body.style.backgroundColor==="white")
+	{
+		nama.style.color="#87CEEB";
+		deskrip[0].style.color='white';
+		judulLink.style.color='white';
+	body.style.backgroundColor='black';
+
+	lampu[0].style.backgroundImage="url('last.png')"
+}
+else if(body.style.backgroundColor==="black")
+{
+	nama.style.color="black";
+	deskrip[0].style.color='black';
+		judulLink.style.color='black';
+	body.style.backgroundColor='white';
+	lampu[0].style.backgroundImage="url('1.png')"
+}
+	},3000);
+	
+	
+
+	
+	lampu[0].removeEventListener('click',aktif);
+}
+function reset()
+{
+	setTimeout(function()
+	{
+lampu[0].classList.remove('animate');
+	lampu[0].addEventListener('click',aktif);
+	
+	},3000);
+	
+}
+lampu[0].addEventListener('click',aktif);
+lampu[0].addEventListener('click',reset);
+
+
+//catatan belum bisa mengganti bacground menjadi on atau off
